ef891e28b1d1e1e0af3755b20eb9bf5345562b27
Hamdi Tuğ <hamditugmobil@gmail.com>
2022-04-16 22:02:35 +0300

    floating refactor with forms

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,48 +2,44 @@ import { Link } from "react-router-dom";
 
 function Login(params) {
   return (
-    <div className="col-12">
-      <div className="row mt-5">
-        <div className="d-flex justify-content-center">
-          <div className="col-4">
-            <main className="form-signin">
-              <form>
-                <h1 className="h3 mb-5 fw-light">Giriş yap</h1>
-                <div className="form-floating mb-3">
-                  <input
-                    type="email"
-                    class="form-control"
-                    id="floatingInput"
-                    placeholder="name@example.com"
-                  />
-                  <label for="floatingInput">Eposta Adresi</label>
-                </div>
-                <div className="form-floating">
-                  <input
-                    type="password"
-                    class="form-control"
-                    id="floatingInput"
-                    placeholder="name@example.com"
-                  />
-                  <label for="floatingInput">Şifre</label>
-                </div>
-
-                <div className="checkbox mb-5 mt-3">
-                  <label>
-                    <input type="checkbox" value="remember-me" /> Beni hatırla
-                  </label>
-                </div>
-                <button className="w-100 btn btn-lg btn-primary" type="submit">
-                  Giriş yap
-                </button>
-              </form>
-              <div className="text-center mt-4">
-                <Link to={"/register"}>
-                  <a>Hesabın yoksa hesap oluştur</a>
-                </Link>
+    <div className="row mt-5">
+      <div className="d-flex justify-content-center">
+        <div className="col-12 col-sm-10 col-md-6 col-lg-4">
+          <main className="form-signin">
+            <form>
+              <h1 className="h3 mb-5 fw-light">Giriş yap</h1>
+              <div className="form-floating mb-3">
+                <input
+                  type="email"
+                  className="form-control"
+                  id="floatingInputEmail"
+                  placeholder="name@example.com"
+                />
+                <label htmlFor="floatingInputEmail">Eposta Adresi</label>
               </div>
-            </main>
-          </div>
+              <div className="form-floating">
+                <input
+                  type="password"
+                  className="form-control"
+                  id="floatingInputPassword"
+                  placeholder="name@example.com"
+                />
+                <label htmlFor="floatingInputPassword">Şifre</label>
+              </div>
+
+              <div className="checkbox mb-5 mt-3">
+                <label>
+                  <input type="checkbox" value="remember-me" /> Beni hatırla
+                </label>
+              </div>
+              <button className="w-100 btn btn-lg btn-primary" type="submit">
+                Giriş yap
+              </button>
+            </form>
+            <div className="text-center mt-4">
+              <Link to={"/register"}>Hesabın yoksa hesap oluştur</Link>
+            </div>
+          </main>
         </div>
       </div>
     </div>
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -2,54 +2,52 @@ import { Link } from "react-router-dom";
 
 function Register(params) {
   return (
-    <div className="col-12">
-      <div className="row mt-5">
-        <div className="d-flex justify-content-center">
-          <div className="col-4">
-            <main className="form-signin">
-              <form>
-                <h1 className="h3 mb-5 fw-light">Hesap oluştur</h1>
+    <div className="row mt-5">
+      <div className="d-flex justify-content-center">
+        <div className="col-12 col-sm-10 col-md-6 col-lg-4">
+          <main className="form-signin">
+            <form>
+              <h1 className="h3 mb-5 fw-light">Hesap oluştur</h1>
 
-                <div className="form-floating mb-3">
-                  <input
-                    type="email"
-                    class="form-control"
-                    id="floatingInput"
-                    placeholder="name@example.com"
-                  />
-                  <label for="floatingInput">Eposta adresi</label>
-                </div>
-                <div className="form-floating mb-3">
-                  <input
-                    type="password"
-                    class="form-control"
-                    id="floatingInput"
-                    placeholder="name@example.com"
-                  />
-                  <label for="floatingInput">Şifre</label>
-                </div>
-
-                <div className="form-floating mb-5">
-                  <input
-                    type="password"
-                    class="form-control"
-                    id="floatingInput"
-                    placeholder="name@example.com"
-                  />
-                  <label for="floatingInput">Şifre tekrar</label>
-                </div>
-
-                <button className="w-100 btn btn-lg btn-primary" type="submit">
-                  Hesap oluştur
-                </button>
-              </form>
-              <div className="text-center mt-4">
-                <Link to={"/login"}>
-                  <a>Hesabın varsa giriş yap</a>
-                </Link>
+              <div className="form-floating mb-3">
+                <input
+                  type="email"
+                  className="form-control"
+                  id="floatingInputEmail"
+                  placeholder="name@example.com"
+                />
+                <label htmlFor="floatingInputEmail">Eposta adresi</label>
               </div>
-            </main>
-          </div>
+              <div className="form-floating mb-3">
+                <input
+                  type="password"
+                  className="form-control"
+                  id="floatingInputPassword"
+                  placeholder="name@example.com"
+                />
+                <label htmlFor="floatingInputPassword">Şifre</label>
+              </div>
+
+              <div className="form-floating mb-5">
+                <input
+                  type="password"
+                  className="form-control"
+                  id="floatingInputPasswordRepeat"
+                  placeholder="name@example.com"
+                />
+                <label htmlFor="floatingInputPasswordRepeat">
+                  Şifre tekrar
+                </label>
+              </div>
+
+              <button className="w-100 btn btn-lg btn-primary" type="submit">
+                Hesap oluştur
+              </button>
+            </form>
+            <div className="text-center mt-4">
+              <Link to={"/login"}>Hesabın varsa giriş yap</Link>
+            </div>
+          </main>
         </div>
       </div>
     </div>
