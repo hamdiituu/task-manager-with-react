2ad05292b2f7989e4b3ca857abe227f4d539f7c5
Hamdi Tuğ <hamditugmobil@gmail.com>
2022-04-16 21:52:41 +0300

    login and register form float refactor

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -4,27 +4,36 @@ function Login(params) {
   return (
     <div className="col-12">
       <div className="row mt-5">
-        <div class="d-flex justify-content-center">
-          <div class="col-4">
-            <main class="form-signin">
+        <div className="d-flex justify-content-center">
+          <div className="col-4">
+            <main className="form-signin">
               <form>
-                <h1 class="h3 mb-5 fw-light">Giriş yap</h1>
-
-                <div class="form-floating mb-3">
-                  <input type="email" class="form-control" />
+                <h1 className="h3 mb-5 fw-light">Giriş yap</h1>
+                <div className="form-floating mb-3">
+                  <input
+                    type="email"
+                    class="form-control"
+                    id="floatingInput"
+                    placeholder="name@example.com"
+                  />
                   <label for="floatingInput">Eposta Adresi</label>
                 </div>
-                <div class="form-floating">
-                  <input type="password" class="form-control" />
-                  <label for="floatingPassword">Şifre</label>
+                <div className="form-floating">
+                  <input
+                    type="password"
+                    class="form-control"
+                    id="floatingInput"
+                    placeholder="name@example.com"
+                  />
+                  <label for="floatingInput">Şifre</label>
                 </div>
 
-                <div class="checkbox mb-3 mt-3">
+                <div className="checkbox mb-5 mt-3">
                   <label>
                     <input type="checkbox" value="remember-me" /> Beni hatırla
                   </label>
                 </div>
-                <button class="w-100 btn btn-lg btn-primary" type="submit">
+                <button className="w-100 btn btn-lg btn-primary" type="submit">
                   Giriş yap
                 </button>
               </form>
