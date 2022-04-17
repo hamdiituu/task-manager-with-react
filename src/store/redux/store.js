4f1431337aada632f9cf7af35ffa4e46f6f0df2c
Hamdi Tuğ <hamditugmobil@gmail.com>
2022-04-18 01:08:09 +0300

    redux persist add

--- src/store/redux/store.js
+++ src/store/redux/store.js
@@ -1,13 +1,30 @@
 import thunkMiddleware from "redux-thunk";
+import { throttle } from "lodash";
+import { loadState, saveState } from "../../utils/localStorage";
 import { createStore, applyMiddleware, combineReducers } from "redux";
 import loginReducer from "../../pages/Login/redux/reducer";
 import registerSlice from "../../pages/Register/slice";
+
+const localState = loadState();
+
 const reducers = combineReducers({
   //reducers
   login: loginReducer,
   register: registerSlice,
 });
 
-const store = createStore(reducers, applyMiddleware(thunkMiddleware));
+const store = createStore(
+  reducers,
+  localState,
+  applyMiddleware(thunkMiddleware)
+);
+
+store.subscribe(
+  throttle(() => {
+    saveState({
+      login: store.getState().login,
+    });
+  }, 1000)
+);
 
 export default store;
---
+++ src/utils/localStorage.js
@@ -0,0 +1,44 @@
+const getKey = (key) => {
+  return `${"react-local"}-${key}`;
+};
+
+export function setItem(key, value) {
+  localStorage.setItem(getKey(key), value);
+}
+
+export function getItem(key, callback = null) {
+  let localItem = localStorage.getItem(getKey(key));
+  if (callback) {
+    localItem = callback(localItem);
+  }
+  return localItem;
+}
+
+export function removeItem(key) {
+  return localStorage.removeItem(getKey(key));
+}
+
+export function clear() {
+  return localStorage.clear();
+}
+
+export const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem(getKey("app-state"));
+    if (serializedState === null) {
+      return undefined;
+    }
+    return JSON.parse(serializedState);
+  } catch (err) {
+    return undefined;
+  }
+};
+
+export const saveState = (state) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem(getKey("app-state"), serializedState);
+  } catch {
+    console.log("useLocaleStorage error!!");
+  }
+};
